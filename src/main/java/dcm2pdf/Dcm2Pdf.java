ea1fe36880bee5770fba03cac7b6af609ec1f34b
edu <edu@edu-pc>
2014-10-08 14:31:56 +0100

    convertion to pdf beta

--- src/main/java/dcm2pdf/Dcm2Pdf.java
+++ src/main/java/dcm2pdf/Dcm2Pdf.java
@@ -10,6 +10,8 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import java.awt.image.BufferedImage;
 
@@ -20,6 +22,8 @@ import javax.imageio.stream.ImageInputStream;
 import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
 
  
+
+
 
 /**
  * Data type that makes a conversion from a DICOM file format to PDF.
@@ -32,6 +36,7 @@ public class Dcm2Pdf {
     /** Path to the resulting PDF file. */
     public static final String PDF = "result.pdf";
     public static final String DCM = "image.dcm";
+    public static final float FACTOR =  0.5f;
  
     /**
      * Creates a PDF file: hello.pdf
@@ -59,46 +64,79 @@ public class Dcm2Pdf {
     public boolean createPdf(String pdfName, String dcmName)
 	throws DocumentException, IOException {
         
+        
+        int width, height, frameIndex = 0;
+        
         /* setup the DICOM reader */
         ImageReader reader = ImageIO.getImageReadersByFormatName("DICOM").next();
         
         /* check image reader */
-	if (reader == null)
+	if(reader == null)
             return false;
         
-        DicomImageReadParam readParams = (DicomImageReadParam) reader.getDefaultReadParam();
+        /* read dicom image parameters */
         
+        DicomImageReadParam readParams = (DicomImageReadParam) reader.getDefaultReadParam();
         File dcmFile = new File(dcmName);
         
+        /* creation of a stream */
         
-        /* */
         ImageInputStream inStream = ImageIO.createImageInputStream(dcmFile);
         reader.setInput(inStream);
-        
-        int frameIndex = 0;
-        
-        
-        /* read the specified frame from the file */
-        BufferedImage bufferImage = reader.read(frameIndex, readParams);
         inStream.close();
+        
+        /* read the dcmfile to a buffer */
+        
+        BufferedImage bufferImage = reader.read(frameIndex, readParams);
+        
+        width = (int) ((bufferImage.getWidth()) * FACTOR );
+        height = (int) ((bufferImage.getHeight()) * FACTOR );
+        System.out.println(width);
+        System.out.println(height);
+        
+        /* resize */
+        
+        BufferedImage bi = resize(bufferImage, width, height);
         
         /* create a document */
         Document document = new Document();
         
-        /* Get the document instance */
+        /* get the document instance */
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(pdfName));
         
         /* open the document */
         document.open();
         
+        /* process the image to the document */
         PdfContentByte pdfcb = new PdfContentByte(writer);
-        
-        Image image = Image.getInstance(pdfcb, bufferImage, 1);
-        
+        Image image = Image.getInstance(pdfcb, bi, 1);
         document.add(image);
         
         /* close the document */
         document.close();
         return true;
+    }
+    
+    /**
+     * Resize the Image to the specific size.
+     * 
+     * @param img img to be resizable
+     * @param width new width
+     * @param height new height
+     * @return image with the new size
+     */
+    
+    
+    public BufferedImage resize(BufferedImage img, int width, int height)
+    {
+        BufferedImage bi = new BufferedImage(width, height, img.getType());
+        
+        /* draw a  */
+        Graphics2D g = bi.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(img, 0, 0, width, height, 0, 0, img.getWidth(), img.getHeight(), null);
+        g.dispose();
+        
+        return bi;
     }
 }
